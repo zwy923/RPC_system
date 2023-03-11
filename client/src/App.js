d2b19c3e98472f3da8372ed94606f58c02d2adc1
zwy923 <811739729@qq.com>
2023-03-11 18:49:03 +0200

    init03

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
+  const [xmlData, setXmlData] = useState('');
+  const [topic, setTopic] = useState('');
+  const [noteName, setNoteName] = useState('');
+  const [noteText, setNoteText] = useState('');
+  const [noteTimestamp, setNoteTimestamp] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleCreateNote = () => {
+    axios.post('localjost:1234/notes', {
+      xmlData: xmlData,
+      topic: topic,
+      noteName: noteName,
+      noteText: noteText,
+      noteTimestamp: noteTimestamp
+    })
+      .then(response => {
+        setXmlData(response.data);
+        alert('Note created successfully');
+      })
+      .catch(error => {
+        console.error(error);
+        alert('Error creating note');
+      });
+  };
+
+  const handleSearchNotes = () => {
+    axios.get('/notes', {
+      params: {
+        xmlData: xmlData,
+        topic: topic
+      }
+    })
+      .then(response => {
+        setSearchResults(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+        alert('Error searching notes');
+      });
+  };
+
+  const handleSearchWikipedia = () => {
+    axios.get('/wikipedia', {
+      params: {
+        searchTerm: searchTerm
+      }
+    })
+      .then(response => {
+        setSearchResults(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+        alert('Error searching Wikipedia');
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextField
+        label="XML Data"
+        multiline
+        rows={4}
+        value={xmlData}
+        onChange={event => setXmlData(event.target.value)}
+      />
+      <TextField
+        label="Topic"
+        value={topic}
+        onChange={event => setTopic(event.target.value)}
+      />
+      <TextField
+        label="Note Name"
+        value={noteName}
+        onChange={event => setNoteName(event.target.value)}
+      />
+      <TextField
+        label="Note Text"
+        multiline
+        rows={4}
+        value={noteText}
+        onChange={event => setNoteText(event.target.value)}
+      />
+      <TextField
+        label="Note Timestamp"
+        value={noteTimestamp}
+        onChange={event => setNoteTimestamp(event.target.value)}
+      />
+      <Button variant="contained" onClick={handleCreateNote}>Create Note</Button>
+      <br/><br/>
+      <TextField
+        label="Search Topic"
+        value={topic}
+        onChange={event => setTopic(event.target.value)}
+      />
+      <Button variant="contained" onClick={handleSearchNotes}>Search Notes</Button>
+      <br/><br/>
+      <TextField
+        label="Search Wikipedia"
+        value={searchTerm}
+        onChange={event => setSearchTerm(event.target.value)}
+      />
+      <Button variant="contained" onClick={handleSearchWikipedia}>Search Wikipedia</Button>
+      <br/><br/>
+      {searchResults.length > 0 && (
+        <div>
+          <h3>Search Results:</h3>
+          <ul>
+            {searchResults.map((result, index) => (
+              <li key={index}>{result}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
+  
 }
 
 export default App;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,91 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const xml2js = require('xml2js');
+const axios = require('axios');
+
+
+// Set up route for creating new notes
+router.post('/notes', (req, res) => {
+  // Parse the XML data from the mock database file
+  xml2js.parseString(req.body.xmlData, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error parsing XML data');
+    } else {
+      // Check if the topic already exists in the XML file
+      const topic = req.body.topic;
+      const note = {
+        name: req.body.noteName,
+        text: req.body.noteText,
+        timestamp: req.body.noteTimestamp
+      };
+      let topicExists = false;
+      for (let i = 0; i < result.data.topic.length; i++) {
+        if (result.data.topic[i].$.name === topic) {
+          topicExists = true;
+          result.data.topic[i].note.push(note);
+          break;
+        }
+      }
+      // If the topic doesn't exist, create a new XML entry for it
+      if (!topicExists) {
+        const newTopic = {
+          $: {
+            name: topic
+          },
+          note: [note]
+        };
+        result.data.topic.push(newTopic);
+      }
+      // Convert the XML data back to a string and send it in the response
+      const builder = new xml2js.Builder();
+      const newXmlData = builder.buildObject(result);
+      res.send(newXmlData);
+    }
+  });
 });
 
+// Set up route for retrieving notes based on topic
+router.get('/notes', (req, res) => {
+  // Parse the XML data from the mock database file
+  xml2js.parseString(req.body.xmlData, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error parsing XML data');
+    } else {
+      // Search the XML data for the given topic and return the results
+      const topic = req.query.topic;
+      let notes = [];
+      for (let i = 0; i < result.data.topic.length; i++) {
+        if (result.data.topic[i].$.name === topic) {
+          notes = result.data.topic[i].note;
+          break;
+        }
+      }
+      res.send(notes);
+    }
+  });
+});
+
+// Set up route for querying Wikipedia API for more information on a given topic
+router.get('/wikipedia', (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchTerm}&format=json&origin=*`;
+  axios.get(url)
+    .then(response => {
+      const data = response.data;
+      const searchResults = {
+        searchTerm: data[0],
+        articles: data[1],
+        descriptions: data[2],
+        links: data[3]
+      };
+      res.send(searchResults);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error querying Wikipedia API');
+    });
+});
 module.exports = router;
